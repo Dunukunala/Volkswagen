a05b605207c70eb254802aaf5531f55414b4638a
Dunukunala <34750341+Dunukunala@users.noreply.github.com>
2019-04-29 12:42:28 +0100

    Update RunnerTest.java

--- TaskTest/src/test/java/Steps/RunnerTest.java
+++ TaskTest/src/test/java/Steps/RunnerTest.java
@@ -1,10 +1,14 @@
 package Steps;
 
 import com.cucumber.listener.ExtentProperties;
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import managers.FileReaderManager;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,5 +30,16 @@ public static void setup() {
     ExtentProperties extentProperties = ExtentProperties.INSTANCE;
     extentProperties.setReportPath(System.getProperty("user.dir") + "/target/cucumber-reports/" + repName);
 }
+        @AfterClass
+  public static void writeExtentReport() {
+    Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+    Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+    Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+    Reporter.setSystemInfo("Machine", 	"MacOS 10.13.6 (17G65)");
+    Reporter.setSystemInfo("Selenium", "3.11.0");
+    Reporter.setSystemInfo("Maven", "3.5.3");
+    Reporter.setSystemInfo("Java Version", "1.8.0_92");
+    Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+  }
 
 }
